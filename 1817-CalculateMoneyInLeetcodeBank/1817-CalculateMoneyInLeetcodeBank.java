f4f692d8bd19b2d0db1c3f23c5c3fd2d8014a380
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:11:48 +0530

    Create 1817-CalculateMoneyInLeetcodeBank

---
+++ 1817-CalculateMoneyInLeetcodeBank/1817-CalculateMoneyInLeetcodeBank.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 11:11:49 AM
+class Solution {
+    public int totalMoney(int n) {
+        int t = n, ans = 0;
+        for (int i = 0; i <= n / 7; i++) {
+            int k = i + 1;
+            if (t / 7 == 0) {
+                for (int j = 0; j < t; j++) ans += k++;
+            } else {
+                for (int j = 0; j < 7; j++) ans += k++;
+                t -= 7;
+            }
+        }
+        return ans;
+    }
+}
