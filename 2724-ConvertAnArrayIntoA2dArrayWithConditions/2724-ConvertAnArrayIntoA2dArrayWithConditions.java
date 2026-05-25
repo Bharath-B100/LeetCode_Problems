1c62d5306e9f6b844b9c814a04cbe3476d658534
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:10:09 +0530

    Create 2724-ConvertAnArrayIntoA2dArrayWithConditions

---
+++ 2724-ConvertAnArrayIntoA2dArrayWithConditions/2724-ConvertAnArrayIntoA2dArrayWithConditions.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 11:10:10 AM
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+            ans.get(freq[c]).add(c);
+            freq[c]++;
+        }
+        return ans;
+    }
+}
+
