a461350c292517beeedbaadf0da797e7dc66f390
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:10:19 +0530

    Create 2606-DifferenceBetweenOnesAndZerosInRowAndColumn

---
+++ 2606-DifferenceBetweenOnesAndZerosInRowAndColumn/2606-DifferenceBetweenOnesAndZerosInRowAndColumn.java
@@ -0,0 +1,19 @@
+// Last updated: 5/25/2026, 11:10:19 AM
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int[] rowOnes = new int[grid.length];
+        int[] colOnes = new int[grid[0].length];
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                rowOnes[i] += grid[i][j];
+                colOnes[j] += grid[i][j];
+            }
+        }
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                grid[i][j] = 2 * (rowOnes[i] + colOnes[j]) - grid.length - grid[0].length;
+            }
+        }
+        return grid;
+    }
+}
