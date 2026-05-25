bd5d541191e484827c00bdc23bb1401293f4d488
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:11:52 +0530

    Create 1761-CountSortedVowelStrings

---
+++ 1761-CountSortedVowelStrings/1761-CountSortedVowelStrings.java
@@ -0,0 +1,10 @@
+// Last updated: 5/25/2026, 11:11:53 AM
+class Solution {
+    public int countVowelStrings(int n) {
+        int[][] dp = new int[n + 1][6];
+        for (int i = 1; i <= n; ++i)
+            for (int k = 1; k <= 5; ++k)
+                dp[i][k] = dp[i][k - 1] + (i > 1 ? dp[i - 1][k] : 1);
+        return dp[n][5];
+    }
+}
