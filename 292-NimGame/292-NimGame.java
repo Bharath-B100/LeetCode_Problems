1dbc4a05d997a8caab5f914757af3ae198a38dee
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:15:12 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 5/25/2026, 11:15:13 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+        }
+            return true;
+        }
+    }
