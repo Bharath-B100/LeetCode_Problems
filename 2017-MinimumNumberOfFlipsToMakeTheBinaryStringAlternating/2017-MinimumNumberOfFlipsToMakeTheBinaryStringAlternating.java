409e0a126b36144704f070d3d54ea62798182e65
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:11:16 +0530

    Create 2017-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating

---
+++ 2017-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating/2017-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating.java
@@ -0,0 +1,19 @@
+// Last updated: 5/25/2026, 11:11:17 AM
+class Solution {
+    public int minFlips(String s) {
+        int n = s.length();
+        int res = n;
+        int[] op = {0, 0};
+
+        for (int i = 0; i < n; i++)
+            op[(s.charAt(i) ^ i) & 1]++;
+
+        for (int i = 0; i < n; i++) {
+            op[(s.charAt(i) ^ i) & 1]--;
+            op[(s.charAt(i) ^ (n + i)) & 1]++;
+            res = Math.min(res, Math.min(op[0], op[1]));
+        }
+
+        return res;
+    }
+}
