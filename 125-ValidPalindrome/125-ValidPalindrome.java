c75eff42b1d1a040ae3f3425a729c0b9ef435d4d
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:16:40 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,16 @@
+// Last updated: 5/25/2026, 11:16:40 AM
+class Solution{
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;        
+    }
+}
