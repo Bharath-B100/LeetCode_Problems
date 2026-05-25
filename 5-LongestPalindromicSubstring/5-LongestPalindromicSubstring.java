0688780f2d77fc2b990ea711fd5cefea36c7933f
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:18:26 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -0,0 +1,33 @@
+// Last updated: 5/25/2026, 11:18:27 AM
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0) return "";
+        boolean dp[][] = new boolean[n][n];
+        int max = 1;
+        String res = s.substring(0, 1);
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                max = 2;
+                res = s.substring(i, i + 2);
+            }
+        }
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (len > max) {
+                        max = len;
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
