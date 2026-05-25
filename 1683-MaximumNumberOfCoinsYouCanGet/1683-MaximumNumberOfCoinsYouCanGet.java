aec4506f9f90c240896d8fc6bba7d52946863e69
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:11:59 +0530

    Create 1683-MaximumNumberOfCoinsYouCanGet

---
+++ 1683-MaximumNumberOfCoinsYouCanGet/1683-MaximumNumberOfCoinsYouCanGet.java
@@ -0,0 +1,13 @@
+// Last updated: 5/25/2026, 11:12:00 AM
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int r = piles.length;
+        int count = 0;
+        for (int i = 0; i < piles.length / 3; i++) {
+            count += piles[r - 2];
+            r -= 2;
+        }
+        return count;
+    }
+}
