eed4c16e9dc69ade0a911c306d696ec40702ea57
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:08:41 +0530

    Create 3848-AnalyzeSubscriptionConversion

---
+++ 3848-AnalyzeSubscriptionConversion/3848-AnalyzeSubscriptionConversion.sql
@@ -0,0 +1,8 @@
+-- Last updated: 5/25/2026, 11:08:41 AM
+select user_id, 
+round((avg(case when activity_type = 'free_trial' then activity_duration end)), 2) as trial_avg_duration,
+round((avg(case when activity_type = 'paid' then activity_duration end)), 2) as paid_avg_duration
+from useractivity
+group by user_id
+having paid_avg_duration is not null
+order by user_id;
