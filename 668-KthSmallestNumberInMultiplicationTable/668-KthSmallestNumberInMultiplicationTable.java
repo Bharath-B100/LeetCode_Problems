010c688c4868a456dc5bcb1f102d7d81168e8de5
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:14:01 +0530

    Create 668-KthSmallestNumberInMultiplicationTable

---
+++ 668-KthSmallestNumberInMultiplicationTable/668-KthSmallestNumberInMultiplicationTable.java
@@ -0,0 +1,19 @@
+// Last updated: 5/25/2026, 11:14:02 AM
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int left = 1;
+        int right = m * n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            for (int i = 1; i <= m; i++) {
+                count += Math.min(mid / i, n);
+            }
+            if (count < k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
