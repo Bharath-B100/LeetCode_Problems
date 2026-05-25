67f86f7daaa42fb4a61d65d6b039731d27bb540d
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:12:44 +0530

    Create 1379-ReconstructA2RowBinaryMatrix

---
+++ 1379-ReconstructA2RowBinaryMatrix/1379-ReconstructA2RowBinaryMatrix.java
@@ -0,0 +1,34 @@
+// Last updated: 5/25/2026, 11:12:45 AM
+class Solution {
+    public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
+	    List<List<Integer>> res = new ArrayList<List<Integer>>(){{
+		    add(new ArrayList<Integer>());
+		    add(new ArrayList<Integer>());
+    	}};
+	for(int i=0; i < colsum.length; i++){
+		if(colsum[i] == 2){
+			res.get(0).add(1);
+			res.get(1).add(1);
+			upper--;
+			lower--;
+		}else if(colsum[i] == 1){
+			if(upper > lower){
+				res.get(0).add(1);
+				res.get(1).add(0);
+				upper--;
+			}else{
+				res.get(0).add(0);
+				res.get(1).add(1);
+				lower--;
+    		}
+    		}else{
+	    		res.get(0).add(0);
+	    		res.get(1).add(0);
+	    	}
+	    }
+	    if(lower!=0||upper !=0){
+		    return Collections.emptyList();
+	    }
+	    return res;  
+    }     
+}
