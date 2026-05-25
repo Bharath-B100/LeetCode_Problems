0928a0eff60ddd4aea0061ab1cacdb06ff63de75
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:08:52 +0530

    Create 3766-MaximumMedianSumOfSubsequencesOfSize3

---
+++ 3766-MaximumMedianSumOfSubsequencesOfSize3/3766-MaximumMedianSumOfSubsequencesOfSize3.java
@@ -0,0 +1,13 @@
+// Last updated: 5/25/2026, 11:08:53 AM
+class Solution {
+    public long maximumMedianSum(int[] nums) {
+        Arrays.sort(nums);
+        long r = 0;
+        int n = nums.length;
+        int d = n / 3;
+        for (int i = d; i < n; i += 2) {
+            r += (long) nums[i];
+        }
+        return r;
+    }
+}
