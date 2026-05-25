bb462420f78d15e07c7013792e2199d344b52001
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:17:41 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,20 @@
+// Last updated: 5/25/2026, 11:17:42 AM
+class Solution {
+    public double myPow(double x, int n) {
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        double pow = 1;
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            } 
+                
+            x *= x;
+            n >>>= 1;
+            
+        }
+        return pow;
+    }
+}
