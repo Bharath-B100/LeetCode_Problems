eef10e2c7872f4b27482a44504757e19120ff4a3
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 19:13:42 +0530

    File updated at 5/25/2026, 7:13:44 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.java
+++ 226-InvertBinaryTree/226-InvertBinaryTree.java
@@ -1,31 +1,31 @@
-// Last updated: 5/25/2026, 11:15:41 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        
-        invertTree(root.left);
-        invertTree(root.right);
-        
-        return root;  
-    }        
-}
+// Last updated: 5/25/2026, 7:13:44 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode invertTree(TreeNode root) {
+18        if (root == null) {
+19            return null;
+20        }
+21        TreeNode temp = root.left;
+22        root.left = root.right;
+23        root.right = temp;
+24        
+25        invertTree(root.left);
+26        invertTree(root.right);
+27        
+28        return root;  
+29    }        
+30}
