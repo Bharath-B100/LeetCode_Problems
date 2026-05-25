4409362a20241624cbcfc3b5b806563df8aedc99
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 19:13:20 +0530

    File updated at 5/25/2026, 7:13:21 PM

--- 100-SameTree/100-SameTree.java
+++ 100-SameTree/100-SameTree.java
@@ -1,9 +1,9 @@
-// Last updated: 5/25/2026, 11:17:01 AM
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        return p.val == q.val && isSameTree(p.left, q.left)&& isSameTree(p.right, q.right);
-
-    }
-}
+// Last updated: 5/25/2026, 7:13:21 PM
+1class Solution {
+2    public boolean isSameTree(TreeNode p, TreeNode q) {
+3        if(p==null && q==null) return true;
+4        if(p==null || q==null) return false;
+5        return p.val == q.val && isSameTree(p.left, q.left)&& isSameTree(p.right, q.right);
+6
+7    }
+8}
