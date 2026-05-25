3fd38d72ad05ad3315c04176d3892fbb97c7adcc
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:12:30 +0530

    Create 1440-ConvertIntegerToTheSumOfTwoNoZeroIntegers

---
+++ 1440-ConvertIntegerToTheSumOfTwoNoZeroIntegers/1440-ConvertIntegerToTheSumOfTwoNoZeroIntegers.java
@@ -0,0 +1,13 @@
+// Last updated: 5/25/2026, 11:12:30 AM
+class Solution {
+    public int[] getNoZeroIntegers(int n) { 
+        for(int i=1;i<n;i++)
+        {
+            int left = i;
+            int right=n-left;
+            if(!String.valueOf(left).contains("0")&&!String.valueOf(right).contains("0"))
+            return new int[] {left,right};
+        }
+        return new int[0];
+    }
+}
