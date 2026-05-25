f67f3b5663b09a85ce36a1894e6b3f031ec46ac7
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:09:31 +0530

    Create 3336-WaterBottlesIi

---
+++ 3336-WaterBottlesIi/3336-WaterBottlesIi.java
@@ -0,0 +1,14 @@
+// Last updated: 5/25/2026, 11:09:32 AM
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottleDrunk = numBottles;
+        int emptyBottles = numBottles;
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange; 
+            numExchange++;               
+            bottleDrunk++;               
+            emptyBottles++;              
+        }
+        return bottleDrunk;
+    }
+}
