5ae48c93f09faf3f90f31899863c57fd18e07263
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:18:03 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.java
@@ -0,0 +1,34 @@
+// Last updated: 5/25/2026, 11:18:04 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count < k) {
+            curr = curr.next;
+            count++;
+        }
+        if (count < k) return head;
+        curr = head;
+        ListNode prev = null, next = null;
+        count = 0;
+        while (curr != null && count < k) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        head.next = reverseKGroup(curr, k);
+        return prev;
+    }
+}
