86b1645716c677c9f9782a5eb204b5794e9821da
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-25 11:14:36 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ 442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 11:14:37 AM
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] < 0) {
+                ans.add(x);
+            }
+            nums[x - 1] *= -1;
+        }
+        return ans;
+    }
+}
