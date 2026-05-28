f42d04426f6c50fc2c501edb1e50df3bad4b5513
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-28 19:20:58 +0530

    File updated at 5/28/2026, 7:20:58 PM

--- 112-PathSum/112-PathSum.java
+++ 112-PathSum/112-PathSum.java
@@ -1,44 +1,44 @@
-// Last updated: 5/25/2026, 11:16:48 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-// class Solution {
-//     public boolean hasPathSum(TreeNode root, int targetSum) {
-//         if (root == null) {
-//             return false;
-//         }
-//         if (root.left == null && root.right == null) {
-//             return targetSum == root.val;
-//         }
-//         boolean leftSum = hasPathSum(root.left, targetSum - root.val);
-//         boolean rightSum = hasPathSum(root.right, targetSum - root.val);
-//         return leftSum || rightSum;
-//     }
-// }
-class Solution{
-    static boolean dfs(TreeNode root,int sum,int target){
-        if(root==null){
-            return false;
-        }
-        if(root.left==null&&root.right==null){
-            return sum+root.val==target;
-        }
-        sum= sum+root.val;
-        return dfs(root.left,sum,target)||dfs(root.right,sum,target);
-    }
-    public boolean hasPathSum(TreeNode root,int targetSum){
-        return dfs(root,0,targetSum);
-    }
-}
+// Last updated: 5/28/2026, 7:20:58 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16// class Solution {
+17//     public boolean hasPathSum(TreeNode root, int targetSum) {
+18//         if (root == null) {
+19//             return false;
+20//         }
+21//         if (root.left == null && root.right == null) {
+22//             return targetSum == root.val;
+23//         }
+24//         boolean leftSum = hasPathSum(root.left, targetSum - root.val);
+25//         boolean rightSum = hasPathSum(root.right, targetSum - root.val);
+26//         return leftSum || rightSum;
+27//     }
+28// }
+29class Solution{
+30    static boolean dfs(TreeNode root,int sum,int target){
+31        if(root==null){
+32            return false;
+33        }
+34        if(root.left==null&&root.right==null){
+35            return sum+root.val==target;
+36        }
+37        sum= sum+root.val;
+38        return dfs(root.left,sum,target)||dfs(root.right,sum,target);
+39    }
+40    public boolean hasPathSum(TreeNode root,int targetSum){
+41        return dfs(root,0,targetSum);
+42    }
+43}
