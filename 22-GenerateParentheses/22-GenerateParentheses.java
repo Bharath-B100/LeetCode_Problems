91a547336e861fb3b7deeb132867ab9346b33344
BHARATH RAJ B <ngtbharath@gmail.com>
2026-05-31 18:39:23 +0530

    File updated at 5/31/2026, 6:39:25 PM

--- 22-GenerateParentheses/22-GenerateParentheses.java
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -1,20 +1,20 @@
-// Last updated: 5/25/2026, 11:18:07 AM
-class Solution {   
-    public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        backtrack(res,"",0,0,n);
-        return res;
-    }
-    private void backtrack(List<String> res, String current,int open,int close,int n){
-        if(current.length() == 2*n){
-            res.add(current);
-            return;
-        }
-        if(open < n){
-            backtrack(res, current + "(" , open +1 ,close, n);
-        }
-        if(close < open){
-            backtrack(res, current + ")" , open, close+1,n);
-        }
-    }
-}
+// Last updated: 5/31/2026, 6:39:25 PM
+1class Solution {   
+2    public List<String> generateParenthesis(int n) {
+3        List<String> res = new ArrayList<>();
+4        backtrack(res,"",0,0,n);
+5        return res;
+6    }
+7    private void backtrack(List<String> res, String current,int open,int close,int n){
+8        if(current.length() == 2*n){
+9            res.add(current);
+10            return;
+11        }
+12        if(open < n){
+13            backtrack(res, current + "(" , open +1 ,close, n);
+14        }
+15        if(close < open){
+16            backtrack(res, current + ")" , open, close+1,n);
+17        }
+18    }
+19}
