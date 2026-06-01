48419a52ae7a183937bce4b4ca350025749b0683
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-01 23:28:36 +0530

    File updated at 6/1/2026, 11:28:36 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,18 +1,19 @@
-// Last updated: 5/25/2026, 11:15:07 AM
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        for (int i = 1; i <= amount; i++)
-            dp[i] = amount + 1;
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
-        }
-        if (dp[amount] == amount + 1) return -1;
-        return dp[amount];
-    }
-}
+// Last updated: 6/1/2026, 11:28:36 PM
+1class Solution {
+2    public int coinChange(int[] coins, int amount) {
+3        int[] dp = new int[amount + 1];
+4        for (int i = 1; i <= amount; i++)
+5            dp[i] = amount + 1;
+6            dp[0] = 0;
+7        for (int i = 1; i <= amount; i++) {
+8            for (int j = 0; j < coins.length; j++) {
+9                if (coins[j] <= i) {
+10                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+11                }
+12            }
+13        }
+14        if (dp[amount] == amount + 1) return -1;
+15        return dp[amount];
+16    }
+17}
+18
