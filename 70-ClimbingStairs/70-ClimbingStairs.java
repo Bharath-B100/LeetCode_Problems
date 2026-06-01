a417972a57f136a0b07f4a1ac1180678987deb99
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-01 23:28:15 +0530

    File updated at 6/1/2026, 11:28:15 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,14 +1,20 @@
-// Last updated: 5/25/2026, 11:17:19 AM
-class Solution {
-    public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        int[] arr = new int[n + 1];
-        arr[1] = 1;
-        arr[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2];
-        }
-        return arr[n];
-    }
-}
+// Last updated: 6/1/2026, 11:28:15 PM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if(n == 1)
+4        {
+5            return 1;
+6        }
+7        if(n == 2)
+8        {
+9            return 2;
+10        }
+11        int[] arr = new int[n+1];
+12            arr[1] = 1;
+13            arr[2] = 2;
+14        for(int i=3;i<=n;i++) {
+15            arr[i] = arr[i-1]+arr[i-2];
+16        }
+17        return arr[n];
+18    }
+19}
