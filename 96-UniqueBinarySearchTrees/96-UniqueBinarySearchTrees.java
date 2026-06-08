cd5194bd8cf5aa3b0b75ef6a655e09b66966c708
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-08 21:27:01 +0530

    File updated at 6/8/2026, 9:27:04 PM

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -1,17 +1,13 @@
-// Last updated: 6/8/2026, 9:23:02 PM
+// Last updated: 6/8/2026, 9:27:04 PM
 1class Solution {
-2    public int numTrees(int n) {
-3        int[] uniqTree = new int[n + 1];
-4        for (int i = 0; i <= n; i++) {
-5            uniqTree[i] = 1;
-6        }
-7        for (int nodes = 2; nodes <= n; nodes++) {
-8            int total = 0;
-9            for (int root = 1; root <= nodes; root++) {
-10                total += uniqTree[root - 1] * uniqTree[nodes - root];
-11            }
-12            uniqTree[nodes] = total;
-13        }
-14        return uniqTree[n];        
-15    }
-16}
+2    public int alternatingSum(int[] nums) {
+3        int sum = 0;
+4
+5        for(int i=0;i<nums.length;i++){
+6            if(i%2 == 0)sum+=nums[i];
+7            else sum-=nums[i];
+8        }
+9
+10        return sum;
+11    }
+12}
