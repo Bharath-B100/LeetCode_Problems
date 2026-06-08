f714308f3945cd682336b7e8cbe460c6f796d2fc
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-08 21:30:47 +0530

    File updated at 6/8/2026, 9:30:50 PM

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -1,13 +1,14 @@
-// Last updated: 6/8/2026, 9:27:04 PM
+// Last updated: 6/8/2026, 9:30:50 PM
 1class Solution {
-2    public int alternatingSum(int[] nums) {
-3        int sum = 0;
-4
-5        for(int i=0;i<nums.length;i++){
-6            if(i%2 == 0)sum+=nums[i];
-7            else sum-=nums[i];
-8        }
-9
-10        return sum;
-11    }
-12}
+2    public int numIdenticalPairs(int[] guestList) {
+3        HashMap<Integer, Integer> hm = new HashMap<>();
+4        int ans = 0;
+5        for(int friend:guestList)
+6        {
+7            int friendCount = hm.getOrDefault(friend,0);
+8            ans+=friendCount;
+9            hm.put(friend,friendCount+1);
+10        }
+11        return ans;
+12    }
+13}
