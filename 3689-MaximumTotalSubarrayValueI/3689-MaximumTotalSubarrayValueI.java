46f1c45ceeb5284c7b85ff92bea6066add28b236
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-09 09:30:48 +0530

    File updated at 6/9/2026, 9:30:47 AM

--- 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.java
+++ 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.java
@@ -1,11 +1,8 @@
-// Last updated: 6/9/2026, 9:24:28 AM
+// Last updated: 6/9/2026, 9:30:47 AM
 1class Solution {
 2    public long maxTotalValue(int[] nums, int k) {
-3        long max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
-4        for(int i=0;i<nums.length;i++){
-5            max = Math.max(max,nums[i]);
-6            min = Math.min(min,nums[i]);
-7        }
-8        return k * (max-min);
-9    }
-10}
+3        int n = nums.length-1;
+4        Arrays.sort(nums);
+5        return (long)k * (nums[n]-nums[0]);
+6    }
+7}
