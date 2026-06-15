4ec367cc21af6686a3e8afcf78b9ea76f22b47f6
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-15 21:17:10 +0530

    File updated at 6/15/2026, 9:17:11 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,33 +1,13 @@
-// Last updated: 5/25/2026, 7:01:17 PM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13        if(head==null){
-14            return null;
-15        }
-16        ListNode slow= head;
-17        ListNode fast= head;
-18
-19        for(int i =0;i<n;i++){
-20            fast=fast.next;
-21        }
-22        if(fast==null){
-23            return head.next;
-24        }
-25        while(fast.next!=null){
-26            slow= slow.next;
-27            fast= fast.next;
-28        } 
-29        slow.next=slow.next.next;
-30        return head;
-31    }
-32}
+// Last updated: 6/15/2026, 9:17:11 PM
+1class Solution {
+2    public int rangeBitwiseAnd(int left, int right) {
+3        int cnt = 0;
+4        while (left != right) {
+5            left >>= 1;
+6            right >>= 1;
+7            cnt++;
+8        }
+9        return (left << cnt);
+10    }
+11}
+12
