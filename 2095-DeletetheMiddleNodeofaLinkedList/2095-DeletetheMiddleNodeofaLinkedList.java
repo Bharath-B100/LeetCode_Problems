4ae4326882f4ddee4e3492cd2c44dde0cc3c5336
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-15 14:56:41 +0530

    File updated at 6/15/2026, 2:56:41 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.java
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 6/15/2026, 2:55:44 PM
+// Last updated: 6/15/2026, 2:56:41 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -11,19 +11,19 @@
 10 */
 11class Solution {
 12    public ListNode deleteMiddle(ListNode head) {
-13        ListNode dummy = head;
-14        ListNode slow = head;
-15        ListNode fast = head;
-16        ListNode prev = null;
+13        ListNode slow = head;
+14        ListNode fast = head;
+15        ListNode prev = null;
+16        ListNode dummy = head;
 17        if(head!=null&&head.next==null){
 18            return null;
 19        }
-20        while(fast!=null&&fast.next!=null){
-21            prev=slow;
+20        while(fast!=null && fast.next!=null){
+21            prev = slow;
 22            slow = slow.next;
-23            fast=fast.next.next;
+23            fast = fast.next.next;
 24        }
-25        prev.next=slow.next;
+25        prev.next = slow.next;
 26        return dummy;
 27    }
 28}
