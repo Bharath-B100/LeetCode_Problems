dee1cbb9c323e287ed51c8de29bd8b1829f9ab9c
BHARATH RAJ B <ngtbharath@gmail.com>
2026-06-17 00:47:36 +0530

    File updated at 6/17/2026, 12:47:36 AM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,27 +1,36 @@
-// Last updated: 6/16/2026, 5:49:21 PM
+// Last updated: 6/17/2026, 12:47:36 AM
 1class Solution {
-2    public int largestRectangleArea(int[] heights) {
-3        int maxArea = 0;
-4        int w =0;
-5        int n = heights.length;
-6        Stack <Integer> st = new Stack<>();
-7        for(int i=0;i<=n;i++)
-8        {
-9            while(!st.isEmpty() && ((i==n) || heights[st.peek()]>heights[i]))
-10            {
-11                int h = heights[st.pop()]; 
-12                if(st.isEmpty())
-13                {
-14                    w = i;
-15                }
-16                else{
-17                    w = i-st.peek()-1;
-18                }
-19                maxArea = Math.max(maxArea,h*w);
-20            }
-21            st.push(i);
-22        }
-23        return maxArea;
-24    }
-25}
-26
+2    public String decodeString(String s) {
+3        Stack<Integer> nums = new Stack<>();
+4        Stack<StringBuilder> strs = new Stack<>();
+5
+6        int num = 0;
+7        StringBuilder curr = new StringBuilder();
+8
+9        for (char c : s.toCharArray()) {
+10            if (Character.isDigit(c)) {
+11                num = num * 10 + (c - '0');
+12            } else if (c == '[') {
+13                nums.push(num);
+14                strs.push(curr);
+15                num = 0;
+16                curr = new StringBuilder();
+17            } else if (c == ']') {
+18                StringBuilder temp = strs.pop();
+19                int repeat = nums.pop();
+20
+21                while (repeat > 0) {
+22                    repeat--;
+23                    temp.append(curr);
+24                }
+25
+26                curr = temp;
+27            } else {
+28                curr.append(c);
+29            }
+30        }
+31
+32        return curr.toString();
+33    }
+34}
+35
