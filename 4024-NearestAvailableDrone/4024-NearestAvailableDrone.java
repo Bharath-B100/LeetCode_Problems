51c822dbd185cb27a286236a0dc3ddc802e59165
BHARATH RAJ B <ngtbharath@gmail.com>
2026-08-22 11:04:55 +0530

    File updated at 8/22/2026, 11:04:54 AM

--- 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
+++ 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
@@ -1,17 +1,19 @@
-// Last updated: 8/22/2026, 9:56:53 AM
+// Last updated: 8/22/2026, 11:04:54 AM
 1class Solution {
-2    public int nearestDrone(int[][] drones, int[] target) {
-3        int diff=-1;
-4        int min =Integer.MAX_VALUE;
-5        for(int i=0;i<drones.length;i++){
-6           int sum = Math.abs(drones[i][0]-target[0])+Math.abs(drones[i][1]-target[1]);
-7            if(sum<=drones[i][2]){
-8                if(sum<min){
-9                    min = sum;
-10                    diff = i;
-11                }
-12            }
-13        }
-14        return diff;
-15    }
-16}
+2    public int minPenalty(int period, int[] lights, int[] arrivalTime) {
+3        int wt = 0;
+4        int max = 0;
+5        for(int light:lights) {
+6            max = Math.max(max, light);
+7        }
+8        for(int t:arrivalTime) {
+9            if(t>=max) {
+10                t =t % period;
+11                if(t>=max) {
+12                    wt = Math.max(wt, period - t);
+13                }
+14            }
+15        }
+16        return wt;
+17    }
+18}
