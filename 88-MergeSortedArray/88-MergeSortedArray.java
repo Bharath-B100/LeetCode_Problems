8b1d448d001262b4512e1f38643c558a0760f86b
BHARATH RAJ B <ngtbharath@gmail.com>
2026-08-28 00:40:58 +0530

    File updated at 8/28/2026, 12:40:58 AM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,11 +1,10 @@
-// Last updated: 5/25/2026, 11:17:09 AM
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m;
-        for (int j=0;j<n;j++) {
-            nums1[i] = nums2[j];
-            i++;
-        }
-        Arrays.sort(nums1);
-    }
-}
+// Last updated: 8/28/2026, 12:40:58 AM
+1class Solution {
+2    public void merge(int[] nums1, int m, int[] nums2, int n) {
+3        for(int j=0;j<n;j++) {
+4            nums1[m] = nums2[j];
+5            m++;
+6        }
+7        Arrays.sort(nums1);
+8    }
+9}
