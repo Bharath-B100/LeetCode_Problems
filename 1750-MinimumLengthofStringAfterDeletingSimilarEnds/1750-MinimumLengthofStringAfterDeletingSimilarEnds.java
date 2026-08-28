d1e9155d778d38b1ba225513bcf38aa7fd95f93b
BHARATH RAJ B <ngtbharath@gmail.com>
2026-08-28 13:50:14 +0530

    File updated at 8/28/2026, 1:50:16 PM

--- 1750-MinimumLengthofStringAfterDeletingSimilarEnds/1750-MinimumLengthofStringAfterDeletingSimilarEnds.java
+++ 1750-MinimumLengthofStringAfterDeletingSimilarEnds/1750-MinimumLengthofStringAfterDeletingSimilarEnds.java
@@ -1,23 +1,20 @@
-// Last updated: 8/28/2026, 12:13:16 PM
+// Last updated: 8/28/2026, 1:50:16 PM
 1class Solution {
-2    public int minimumLength(String s) {
-3        int i = 0;
-4        int j = s.length() - 1;
-5        while(i < j) {
-6            if(s.charAt(i)!=s.charAt(j)) {
-7                break;
-8            }
-9            if(s.charAt(i)==s.charAt(j)) {
-10                char ch=s.charAt(i);
-11                while(i <= j && s.charAt(i) == ch) {
-12                    i++;
-13                }
-14                while(j >= i && s.charAt(j) == ch) {
-15                    j--;
-16                }
-17            }
-18        }
-19        return j - i + 1;
-20    }
-21}
-22
+2    public int findMinDifference(List<String> t) {
+3        int arr[] = new int[t.size()];
+4        for(int i=0;i<t.size();i++){
+5            String s = t.get(i);
+6            int h = Integer.parseInt(s.substring(0,2));
+7            int m = Integer.parseInt(s.substring(3,5));
+8            if(h+m==0) h = 24;
+9            arr[i] = (h*60)+ m;
+10        }
+11        Arrays.sort(arr);
+12        int min = Integer.MAX_VALUE;
+13        for(int i=1;i<t.size();i++){
+14            min = Math.min(Math.abs(arr[i-1]-arr[i]),min);
+15        }
+16        min = Math.min(min,Math.abs(((24*60)-arr[arr.length-1])+arr[0]));
+17        return min;
+18    }
+19}
